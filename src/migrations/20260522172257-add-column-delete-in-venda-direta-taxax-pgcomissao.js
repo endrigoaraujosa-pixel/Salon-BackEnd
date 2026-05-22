6d1a19fca69050d0cf1c734f9517cc9601a726a3
Itamar Nascimento <itamarasa@gmail.com>
2026-05-22 14:34:57 -0300

    feat: add soft delete columns to vendas_diretas, taxas_cartao, and pagamentos_comissao tables

--- src/migrations/20260522172257-add-column-delete-in-venda-direta-taxax-pgcomissao.js
+++ src/migrations/20260522172257-add-column-delete-in-venda-direta-taxax-pgcomissao.js
@@ -1,7 +1,4 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
+export default {
   async up(queryInterface, Sequelize) {
     const tables = [
       'vendas_diretas',
