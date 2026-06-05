3271e25ab7019d71733c9fb17fd9007bd6c4cc37
Itamar Nascimento <itamarasa@gmail.com>
2026-06-05 16:21:12 -0300

    feat: initialize Express backend server and SQLite database setup

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const app = express();
 
 // Middleware
 const allowedOrigins = [
-  'https://www.salonstudio.com.br',
+  'https://salonstudio.com.br',
   'http://localhost:4000',
   'http://localhost:3000',
   'http://127.0.0.1:4000'
